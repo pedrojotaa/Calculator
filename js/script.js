c643479f94f7f9d277145f02e64c83510eeafb36
Joao Pedro <eng.joao.pedro@outlook.com>
2021-10-08 14:11:25 -0300

    creating variables to buttons

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let zero = document.querySelector('#0')
+let um = document.querySelector('#1')
+let dois = document.querySelector('#2')
+let tres = document.querySelector('#3')
+let quatro = document.querySelector('#4')
+let cinco = document.querySelector('#5')
+let seis = document.querySelector('#6')
+let sete = document.querySelector('#7')
+let oito = document.querySelector('#8')
+let novo = document.querySelector('#9')
+
+let multiplica = document.querySelector('')
+let divide = document.querySelector('')
+let soma = document.querySelector('')
+let subtrai = document.querySelector('')
+
+let resultado = document.querySelector('#resultado')
+
