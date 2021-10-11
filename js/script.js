80503a5af566d2de674327b8dd691a764b60e2dd
Joao Pedro <eng.joao.pedro@outlook.com>
2021-10-11 07:25:12 -0300

    printing on screen the value of numbers

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
-let zero = document.querySelector('#0')
-let um = document.querySelector('#1')
-let dois = document.querySelector('#2')
-let tres = document.querySelector('#3')
-let quatro = document.querySelector('#4')
-let cinco = document.querySelector('#5')
-let seis = document.querySelector('#6')
-let sete = document.querySelector('#7')
-let oito = document.querySelector('#8')
-let novo = document.querySelector('#9')
+let zero = document.querySelector('#zero')
+let um = document.querySelector('#um')
+let dois = document.querySelector('#dois')
+let tres = document.querySelector('#tres')
+let quatro = document.querySelector('#quatro')
+let cinco = document.querySelector('#cinco')
+let seis = document.querySelector('#seis')
+let sete = document.querySelector('#sete')
+let oito = document.querySelector('#oito')
+let nove = document.querySelector('#nove')
 
-let multiplica = document.querySelector('')
-let divide = document.querySelector('')
-let soma = document.querySelector('')
-let subtrai = document.querySelector('')
-
+let multiplica = document.querySelector('#multiplica')
+let divide = document.querySelector('#divide')
+let soma = document.querySelector('#soma')
+let subtrai = document.querySelector('#subtrai')
 let resultado = document.querySelector('#resultado')
 
+let telaResultado = document.querySelector('#telaResultado')
+
+
+
+soma.addEventListener('click', (valor) => {
+    valor = soma.value
+    telaResultado.innerHTML += `<span> ${valor} </span>`
+    if()
+})
+
+//um.addEventListener('click', (valor) => {
+//    valor = um.value
+//    telaResultado.innerHTML += `<span> ${valor} </span>`
+//})
+
+function umClick() {
+    let umValor = um.value
+    let valor = parseInt(umValor)
+    telaResultado.innerHTML += `<span> ${valor} </span>`
+}
+
+
+resultado.addEventListener('click', () => {
+    let um = um.value
+    let soma = um(valor) + um(valor)
+
+    telaResultado.innerHTML = `<span> ${soma} </span>`
+})
