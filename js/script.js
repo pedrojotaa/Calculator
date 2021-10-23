2d06e874f267aa485f092edc1fd282ee3148a98a
Joao Pedro <eng.joao.pedro@outlook.com>
2021-10-23 05:02:40 -0700

    making improvements

--- js/script.js
+++ js/script.js
@@ -1,45 +1,10 @@
-let zero = document.querySelector('#zero')
-let um = document.querySelector('#um')
-let dois = document.querySelector('#dois')
-let tres = document.querySelector('#tres')
-let quatro = document.querySelector('#quatro')
-let cinco = document.querySelector('#cinco')
-let seis = document.querySelector('#seis')
-let sete = document.querySelector('#sete')
-let oito = document.querySelector('#oito')
-let nove = document.querySelector('#nove')
-
-let multiplica = document.querySelector('#multiplica')
-let divide = document.querySelector('#divide')
-let soma = document.querySelector('#soma')
-let subtrai = document.querySelector('#subtrai')
-let resultado = document.querySelector('#resultado')
-
+let buts = document.querySelectorAll('.but')
 let telaResultado = document.querySelector('#telaResultado')
 
-
-
-soma.addEventListener('click', (valor) => {
-    valor = soma.value
-    telaResultado.innerHTML += `<span> ${valor} </span>`
-    if()
-})
-
-//um.addEventListener('click', (valor) => {
-//    valor = um.value
-//    telaResultado.innerHTML += `<span> ${valor} </span>`
-//})
-
-function umClick() {
-    let umValor = um.value
-    let valor = parseInt(umValor)
-    telaResultado.innerHTML += `<span> ${valor} </span>`
-}
-
-
-resultado.addEventListener('click', () => {
-    let um = um.value
-    let soma = um(valor) + um(valor)
-
-    telaResultado.innerHTML = `<span> ${soma} </span>`
+buts.forEach((but) => {
+    but.addEventListener('click', (event) => {
+        let click = event.target
+        let position = click.id
+        console.log(position)
+    })
 })
